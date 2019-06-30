9cb247758af7d6455c0561f9a8b0e65b24ae96a1
Anderson Salgado <anderson.desen@gmail.com>
2019-06-29 23:30:41 -0300

    Commitando.

--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -12,46 +12,59 @@ namespace SimpleBotCore.Logic
     {
         public string Reply(SimpleMessage message)
         {
-            var cliente = new MongoClient("mongodb://localhost:27017");
 
-            var db = cliente.GetDatabase("15Net");
-
-            var col = db.GetCollection<SimpleMessage>("message");
-
-            var colContador = db.GetCollection<UsuarioContador>("usuarioContador");
-
-            var contador = colContador.Find(x => x.Id.Equals(message.Id));
-
-            var usuarioContador = new UsuarioContador();
-            //Existe contador
-            if (contador.Any())
+            try
             {
-                var contadorGravado = contador.FirstOrDefault();
-                contadorGravado.contador++;
-                //var filterContador = Builders<UsuarioContador>.Filter.Where(x => x.Id.Equals(message.Id));
-                colContador.DeleteMany(FilterDefinition<UsuarioContador>.Empty);
-                colContador.InsertOne(contadorGravado);
+                var cliente = new MongoClient("mongodb://172.17.0.2:27017");
+
+                var db = cliente.GetDatabase("15Net");
+
+                var col = db.GetCollection<SimpleMessage>("message");
+
+                var colContador = db.GetCollection<UsuarioContador>("usuarioContador");
+
+                var contador = colContador.Find(x => x.Id.Equals(message.Id));
+
+                var usuarioContador = new UsuarioContador();
+                //Existe contador
+                if (contador.Any())
+                {
+                    var contadorGravado = contador.FirstOrDefault();
+                    contadorGravado.contador++;
+                    //var filterContador = Builders<UsuarioContador>.Filter.Where(x => x.Id.Equals(message.Id));
+                    colContador.DeleteMany(FilterDefinition<UsuarioContador>.Empty);
+                    colContador.InsertOne(contadorGravado);
+                }
+                else
+                {
+
+                    //Zerar contador
+                    colContador.DeleteMany(FilterDefinition<UsuarioContador>.Empty);
+
+                    usuarioContador.Id = message.Id;
+                    usuarioContador.contador++;
+
+                    colContador.InsertOne(usuarioContador);
+
+
+                }
+
+
+                message.Id = $"{message.Id}_{DateTime.Now}";
+
+                col.InsertOne(message);
+
+                return $"{message.User} disse '{message.Text}'";
             }
-            else
+            catch (Exception ex)
             {
-
-                //Zerar contador
-                colContador.DeleteMany(FilterDefinition<UsuarioContador>.Empty);
-
-                usuarioContador.Id = message.Id;
-                usuarioContador.contador++;
-
-                colContador.InsertOne(usuarioContador);
-
-                
+                Console.WriteLine(ex.Message);
+                //throw;
             }
+
+
+            return "";
             
-
-            message.Id = $"{message.Id}_{DateTime.Now}";
-
-            col.InsertOne(message);
-
-            return $"{message.User} disse '{message.Text}'";
         }
 
     }
